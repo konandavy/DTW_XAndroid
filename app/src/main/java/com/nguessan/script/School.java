ded97d3e76cc507f744c600350a60e4ef1d10a4e
konandavy <davynguessan28@gmail.com>
2017-03-05 01:04:39 -0600

    Initial Commit

---
+++ app/src/main/java/com/nguessan/script/School.java
@@ -0,0 +1,20 @@
+package com.nguessan.script;
+
+import android.view.View;
+
+
+/**
+ * Created by kevinlallen1 on 3/4/2017.
+ */
+
+
+public class School {
+
+    int check = 0;
+
+    public void onClick(View box){
+        check = 1;
+
+    }
+
+}
