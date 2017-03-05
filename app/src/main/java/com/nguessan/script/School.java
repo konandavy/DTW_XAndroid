0ba0ed6f6f29eb7340019fbb7a3ca685ae1dd6df
konandavy <davynguessan28@gmail.com>
2017-03-05 05:25:52 -0600

    Added map intent button to school.xml

--- app/src/main/java/com/nguessan/script/School.java
+++ app/src/main/java/com/nguessan/script/School.java
@@ -1,16 +1,50 @@
 package com.nguessan.script;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class School extends Activity {
     CheckBox checkbox1,checkBox2;
+    Button Butt1;
+
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_school);
 
+
+
+        Butt1 = (Button) findViewById(R.id.bt1);
+
+        Butt1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+
+
+                Intent intent = new Intent(School.this,MapsActivity.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
     }
+
+
+
+
+
 }
