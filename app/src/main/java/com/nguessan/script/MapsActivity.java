772417c04516b72176dab376e9c9c85fb0be3c68
Eidolynn <nath4nr@gmail.com>
2017-03-09 19:25:08 -0600

    added some images to drawable folder

--- app/src/main/java/com/nguessan/script/MapsActivity.java
+++ app/src/main/java/com/nguessan/script/MapsActivity.java
@@ -91,7 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
         //Initialize Google Play Services
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
